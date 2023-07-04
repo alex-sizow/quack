00284ef2f773b8d56fe5faa4a78aa4d7063c59c0
Alex <alex_sizow@outlook.com>
2023-07-04 23:58:36 +0300

    Initial commit

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+	<div>
+		<div
+			class="p-2 m-2 rounded-full transition-colors hover:bg-red-200 w-min">
+			<Nuxt-Link to="/"
+				><div class="w-8 h-8"><logo-twitter /></div
+			></Nuxt-Link>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
