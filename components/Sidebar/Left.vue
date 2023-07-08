6145f6b069cbc5452ee641aa796890dc29c8e272
Alex <alex_sizow@outlook.com>
2023-07-08 23:48:00 +0300

    tailwind and navbar config

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,13 +1,18 @@
-<script setup></script>
+<script setup>
+import { HomeIcon } from '@heroicons/vue/24/solid';
+const { defaultTransition } = useTailwindConfig();
+</script>
 
 <template>
-	<div>
+	<div class="flex flex-col h-screen">
 		<div
-			class="p-2 m-2 rounded-full transition-colors hover:bg-red-200 w-min">
+			class="p-2 my-2 rounded-full hover:bg-red-200 w-min"
+			:class="defaultTransition">
 			<Nuxt-Link to="/"
 				><div class="w-8 h-8"><logo-twitter /></div
 			></Nuxt-Link>
 		</div>
+		<HomeIcon />
 	</div>
 </template>
 
