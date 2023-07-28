8ebf41f8c19156ec7210d19421dd51c87726cd2c
Alex <alex_sizow@outlook.com>
2023-07-28 23:32:17 +0300

    layout fix

--- app.vue
+++ app.vue
@@ -6,10 +6,24 @@ const darkMode = ref(false);
 	<div :class="{ dark: darkMode }">
 		<div class="bg-white dark:bg-dim-900">
 			<div class="min-h-full">
+				<!--  Left sidebar  -->
 				<div
 					class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
-					<div class="md:block xs-col-span-1 xl:col-span-2">
+					<div
+						class="hidden md:block md:col-span-3 xl:col-span-4">
 						<div class="sticky top-0"><SidebarLeft /></div>
+					</div>
+					<!-- Main content-->
+					<main
+						class="col-span-12 bg-red-500 md:col-span-8 xl:col-span-6">
+						<h1>main</h1>
+					</main>
+					<!-- Right sidebar -->
+					<div
+						class="hidden bg-blue-500 md:block xl:col-span-4 md:col-span-3">
+						<div class="sticky top-0">
+							<h1>Right Sidebar</h1>
+						</div>
 					</div>
 				</div>
 			</div>
